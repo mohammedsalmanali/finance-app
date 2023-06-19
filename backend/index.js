7c780207298e9b31523d82c3876361123ab6d49b
Mohammed Salman Ali <mohammedsalmanali010@gmail.com>
2023-06-19 09:44:30 +0000

    commented out middleware

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,17 @@
 // Import required modules
-import express, { json, urlencoded } from 'express';
+const express = require('express');
 const app = express();
-const port = 3000; // Set the port number as per your preference
-
+const port = 6000; // Set the port number as per your preference
+const { json } = require('express');
+const { urlencoded } = require('express');
 // Middleware
-app.use(json()); // Parse JSON requests
-app.use(urlencoded({ extended: true })); // Parse URL-encoded requests
+//app.use(json()); // Parse JSON requests
+//app.use(urlencoded({ extended: true })); // Parse URL-encoded requests
 
 // Define API endpoints and corresponding route handlers
 
 // Example endpoint
-app.get('/api/users', (req, res) => {
-  // Handle the GET request for /api/users
-  // Implement the logic to fetch users from the database or any other data source
-  // Return the users as a response
-  const users = [
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Jane Smith' }
-  ];
-  res.json(users);
-});
-
+app.get('/', (req, res) => res.send('Hello World'))
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
